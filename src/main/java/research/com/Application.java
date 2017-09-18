9b0375a95cdb7c1163e2b2f53f0a04ac81b8064d
baopham <ngocbaopt@gmail.com>
2017-09-18 15:41:00 -0700

    Add one more success step

--- src/main/java/research/com/Application.java
+++ src/main/java/research/com/Application.java
@@ -3,6 +3,6 @@ package research.com;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World. Today is September 18th");
     }
 }
